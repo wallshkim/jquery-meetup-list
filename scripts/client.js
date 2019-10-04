0357671a0472363cae3ab4c3df8e6aca222395c0
Heather Walls <wallshkim@gmail.com>
2019-10-04 12:38:08 -0500

    Decrements totalMeetup value by 1 when deleteButton clicked and updates DOM

--- scripts/client.js
+++ scripts/client.js
@@ -4,16 +4,18 @@ let totalMeetups = 3;
 
 function readyNow() {
     console.log('jquery working!');
-    $('#addMeetupButton').on('click', handleClick);
+    // add event listener for addMeetupButton
+    $('#addMeetupButton').on('click', handleAddGroupClick);
+    // add event listener for deleteButton
     $('#tableBody').on('click', '.deleteButton', handleDelete)
 }
 
-function handleClick(){
+function handleAddGroupClick(){
     console.log('I was clicked');
     // get input values
     let newGroupInput = $('#newGroupInput').val();
     let meetupDateInput = $('#meetupDateInput').val();
-    let deleteButton = '<td><button class="deleteButton" type="button">Delete</button></td>';
+    let deleteButton = '<button class="deleteButton" type="button">Delete</button>';
     // append values to table body
     $('#tableBody').append(`
     <tr>
@@ -21,17 +23,23 @@ function handleClick(){
         <td>${meetupDateInput}</td>
         <td>${deleteButton}</td>
     </tr>`);
+    // increase totalMeetups by 1
+    totalMeetups += 1;
+    // add count of total meetups to total meetups h2
+    $('#totalMeetupsOutput').text(totalMeetups);
     // clear inputs
     $('#newGroupInput').val('');
     $('#meetupDateInput').val('');
-    // increase totalMeetups by 1
-    totalMeetups ++;
-    // add count of total meetups to total meetups h2
-    $('#totalMeetupsOutput').text(totalMeetups);
 }
 
 function handleDelete(){
     console.log('in handleDelete');
+    // get closest tr
     let newTableRow = $(this).closest('tr');
+    // Remove entire tr element
     $(newTableRow).remove();
+    // Decrement totalMeetups
+    totalMeetups --;
+    // Replace totalMeetupsOutput text with new value
+    $('#totalMeetupsOutput').text(totalMeetups);
 }
